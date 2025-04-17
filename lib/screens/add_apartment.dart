4593d3d0ccdc61914b92027c16d675896be46a95
carlkiptoo <carlosiamcarl@gmail.com>
2025-04-17 18:07:45 +0300

    apt name input

--- lib/screens/add_apartment.dart
+++ lib/screens/add_apartment.dart
@@ -50,4 +50,37 @@ class _AddApartmentScreenState extends State<AddApartmentScreen> {
     }
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Apartment'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            // name input
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                labelStyle: Theme.of(context).textTheme.bodyMedium,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Theme.of(context).primaryColor),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Color(0xFFCCCCCC)),
+                  borderRadius: BorderRadius.circular(8),
+                )
+              ),
+            ),
+            const SizedBox(height: 16),
+          ],
+        ),
+      )
+    )
+  }
+
 }
