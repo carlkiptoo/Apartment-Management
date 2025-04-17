387878e5c938c64f84b7cb2df49b0482162f5a68
carlkiptoo <carlosiamcarl@gmail.com>
2025-04-17 17:58:58 +0300

    adding apartments to db

--- lib/screens/add_apartment.dart
+++ lib/screens/add_apartment.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
-import '../theme/app_colors.dart';
+import '../database/db_helper.dart';
 
-class AddApartmentScreen extends StatelessWidget {
+class AddApartmentScreen extends StatefulWidget {
   const AddApartmentScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Add Apartment")),
-      body: const Center(
-        child: Text(
-          'Welcome to the Add Apartment screen!',
-          style: TextStyle(fontSize: 18, color: AppColors.dark),
-        ),
-      ),
-    );
+  State<AddApartmentScreen> createState() => _AddApartmentScreenState();
+
+}
+
+class _AddApartmentScreenState extends State<AddApartmentScreen> {
+  final _nameController = TextEditingController();
+  final _locationController = TextEditingController();
+  final _priceController = TextEditingController();
+  final _imageUrlController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  bool _loading = false;
+
+  Future<void> _submitApartment() async {
+    final apartment = {
+      "name": _nameController.text,
+      "location": _locationController.text,
+      "price": double.tryParse(_priceController.text) ?? 0.0,
+      "imageUrl": _imageUrlController.text,
+      "description": _descriptionController.text,
+    };
+
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      await DBHelper().insertApartment(apartment);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Apartment Saved Successfully')),
+      );
+      _nameController.clear();
+      _locationController.clear();
+      _priceController.clear();
+      _imageUrlController.clear();
+      _descriptionController.clear();
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error saving: $e')),
+      );
+    } finally {
+      setState(() => _loading = false);
+    }
   }
+
 }
