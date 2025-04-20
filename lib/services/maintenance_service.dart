d79b457388f4be7e6ffc611ab0fdba3b3c90b341
carlkiptoo <carlosiamcarl@gmail.com>
2025-04-20 12:43:01 +0300

    add and get all requests

---
+++ lib/services/maintenance_service.dart
@@ -0,0 +1,20 @@
+import 'package:sqflite/sqflite.dart';
+import '../database/db_helper.dart';
+import '../models/maintenance_request.dart';
+
+class MaintenanceService {
+  final dbHelper = DBHelper();
+
+  Future<int> addRequest(MaintenanceRequest request) async {
+    final db = await dbHelper.database;
+    return await db.insert('maintenance_requests', request.toMap());
+  }
+
+  Future<List<MaintenanceRequest>> getAllRequests() async {
+    final db = await dbHelper.database;
+    final maps = await db.query('maintenance_requests');
+
+    return maps.map((map) => MaintenanceRequest.fromMap(map)).toList();
+  }
+
+}
