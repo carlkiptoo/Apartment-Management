e55333ae2b1a4dd06c68c87bfbadbc371632799d
carlkiptoo <carlosiamcarl@gmail.com>
2025-04-23 17:33:28 +0300

    admin login(api integration

--- lib/screens/admin_login_screen.dart
+++ lib/screens/admin_login_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'apartment_list_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../theme/app_colors.dart';
@@ -14,32 +17,86 @@ class _LoginScreenState extends State<LoginScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  final String _adminEmail = 'admin@admin.com';
-  final String _adminPassword = 'admin123';
 
   bool _loading = false;
 
+  // Future<void>  _login() async {
+  //
+  //   final email = _emailController.text.trim();
+  //   final password = _passwordController.text.trim();
+  //   final url = Uri.parse('http://192.168.100.6:5000/api/auth/login');
+  //   final headers = {'Content-Type': 'application/json'};
+  //   final body = jsonEncode({'email': email, 'password': password});
+  //
+  //   try {
+  //     final response = await http.post(url, headers: headers, body: body);
+  //     if (response.statusCode == 200) {
+  //       print('Login success');
+  //     } else {
+  //       print('Login failed ${response.body}');
+  //     }
+  //
+  //   } catch (e) {
+  //     print('Error: ${e}');
+  //   }
+  // }
+
   Future<void> _login() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isLoggedIn', true);
+
+    setState(() {
+      _loading = true;
+    });
+
     final email = _emailController.text.trim();
     final password = _passwordController.text.trim();
 
-    if (email == _adminEmail && password == _adminPassword) {
-      setState(() {
-        _loading = true;
-      });
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setBool('isLoggedIn', true);
+    final url = Uri.parse('http://192.168.100.6:5000/api/auth/login');
 
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (_) => const ApartmentListScreen()),
+    try {
+      final response = await http.post(
+        url,
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({'email': email, 'password': password}),
       );
-    } else {
-      ScaffoldMessenger.of(
-        context,
-      ).showSnackBar(const SnackBar(content: Text('Invalid credentials')));
+      print('Response body: ${response.body}');
+
+      if (response.statusCode == 200) {
+        print('Status code: ${response.statusCode}');
+        if (response.headers['content-type']?.contains('application/json') ?? false){
+          final data = jsonDecode(response.body);
+          final prefs = await SharedPreferences.getInstance();
+          await prefs.setBool('isLoggedIn', true);
+          await prefs.setString('token', data['token']);
+          await prefs.setString('userRole', data['user']['role']);
+          // await prefs.setString('userId', data['user']['_id']);
+        } else {
+          print('Response is not in JSON: ${response.body}');
+        }
+
+
+
+        if (mounted) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (_) => const ApartmentListScreen()),
+          );
+        }
+      } else {
+        final error = jsonDecode(response.body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(error['message'] ?? 'Login failed')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred. Please try again')),
+      );
+    } finally {
+      if (mounted) {
+        setState(() {
+          _loading = false;
+        });
+      }
     }
   }
 
@@ -76,7 +133,8 @@ class _LoginScreenState extends State<LoginScreen> {
               obscureText: true,
             ),
             const SizedBox(height: 20),
-            ElevatedButton(onPressed: _login, child: const Text('Login')),
+            _loading ? const CircularProgressIndicator() : ElevatedButton(onPressed: _login, child: Text('Login'),
+            ),
           ],
         ),
       ),
